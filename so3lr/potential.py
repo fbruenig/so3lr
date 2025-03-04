c7f33286b807e159c6ade458c76f7a428dd3fc47
Florian Brünig <florian.bruenig@fu-berlin.de>
2025-03-04 13:44:20 +0100

    pme electrostatics

--- so3lr/potential.py
+++ so3lr/potential.py
@@ -6,8 +6,9 @@ from mlff.mdx.potential import MLFFPotentialSparse
 
 def make_potential_fn(
     lr_cutoff=12.,
-        dispersion_energy_cutoff_lr_damping=2.,
-        dtype=np.float32
+        dispersion_energy_lr_cutoff_damping=2.,
+        dtype=np.float32,
+        **kwargs
 ):
     package_dir = pathlib.Path(__file__).parent.parent.resolve()
 
@@ -16,8 +17,9 @@ def make_potential_fn(
         from_file=True,
         long_range_kwargs=dict(
             cutoff_lr=lr_cutoff,
-            dispersion_energy_cutoff_lr_damping=dispersion_energy_cutoff_lr_damping,
-            neighborlist_format_lr='ordered_sparse'
+            dispersion_energy_cutoff_lr_damping=dispersion_energy_lr_cutoff_damping,
+            neighborlist_format_lr='ordered_sparse',
+            **kwargs
         ),
         dtype=dtype
     )
